d44b5085e7332090d9d78103eb56941210bcd584
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-03-25 15:26:49 -0400

    edit tasks and bootstrap grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,6 +82,85 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+
+$(this).replaceWith(textInput);
+
+textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function() {
+
+  var text = $(this).val();
+
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  $(this).replaceWith(taskP);
+
+});
+
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  $(this).replaceWith(dateInput);
+
+  // automatically bring up the calendar
+  dateInput.trigger("focus");
+});
+
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    $(this).replaceWith(taskSpan);
+});
+
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -95,7 +174,7 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
-$(".list-group").on("click", "p", function() {
-  var text = $(this).text();
-  console.log(text);
-});
+
+
+
+
